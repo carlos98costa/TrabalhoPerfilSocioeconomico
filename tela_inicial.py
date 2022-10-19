8426953ae73e96d762fc650d131a5adcd191ce53
carlos98costa <46066793+carlos98costa@users.noreply.github.com>
2022-10-19 13:42:02 -0300

    new update

--- tela_inicial.py
+++ tela_inicial.py
@@ -1,12 +1,12 @@
 import PySimpleGUI as sg
 
 
+
 def tela_inicial(theme):
     sg.theme(theme)
-    menu_def = [["Ajuda", ["Sobre", "Equipe", "Documentação", "Sair"]]]
-    layout_inicial = [[sg.MenubarCustom(menu_def, tearoff=False, background_color='gray')],
+    menu_def = [["Ajuda", ["Sobre", "Equipe", "Sair"]]]
+    layout_inicial = [[sg.MenubarCustom(menu_def, tearoff=False, background_color='#404040', bar_background_color='#404040', bar_text_color='white', text_color='white')],
                     [sg.T('Bem-vindo ao programa!', font='_ 30', justification='c', expand_x=True)],
-                      #[sg.Image('logo_fatec.png')],
                     [sg.Ok('Continuar', button_color='gray'), sg.Cancel('Sair', button_color='red')]
 
                  ]
--- testando.py
+++ testando.py
@@ -1,12 +1,16 @@
 import csv
 import pandas as pd
 import tela_1
+
 import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from datetime import datetime, date
 
 import tela_inicial
 
+
+def get_file_list():
+    return listBox
 
 ##CRIAR TELAS
 
@@ -20,10 +24,15 @@ def make_window(theme='Dark'):
 
     sg.theme(theme)
 
+
+
     layout_tela = [[sg.T('SOCIOECONÔMICO', font='18', justification='c', expand_x=True)],
                    [name('Selecione o grafico a ser mostrado'),
-                    sg.LB(values=listBox[0:], key='-LB-', bind_return_key=True, enable_events=True,
-                          no_scrollbar=False, s=(80, 12)), ],
+                    sg.LB(values=get_file_list(), key='-LB-', bind_return_key=True, enable_events=True,
+                          no_scrollbar=False, s=(80, 12), background_color='#4D4B4B'), ],
+                   [name('Buscar grafico pelo nome'),
+                    sg.Input(size=(25, 1), enable_events=True, key='-FILTER-'),
+                    sg.T(size=(15, 1), k='-FILTER NUMBER-')],
                    [sg.Cancel('Voltar', size=(6, 4), button_color='gray'), sg.Cancel('Sair', size=(6, 4), button_color='red')]]
 
     return sg.Window('Perfil Socioeconômico - Gerar graficos', layout_tela, finalize=True, size=(800, 300))
@@ -37,6 +46,7 @@ def calculateAge(birthDate):
 
 # Start of the program...
 janela1, janela2, janela3 = None, None, tela_inicial.tela_inicial('Dark')
+
 
 while True:
     window, event, values = sg.read_all_windows()
@@ -56,12 +66,10 @@ while True:
         break
 
     if window == janela3 and event == 'Sobre':
-        sg.popup('Sobre o programa', 'Programa feito para tratar o arquivo CSV coletado na primeira etapa do trabalho!', font='18', title='Sobre')
+        sg.popup('Sobre o programa', 'Criação do sistema de processamento de dados, com a finalidade de gerar gráficos informativos a partir do arquivo '
+                                     'CSV gerado na primeira etapa do trabalho.', font='18', title='Sobre')
     if window == janela3 and event == 'Equipe':
-        sg.popup('Nossa equipe:','Adriano', 'Carlos Adriano', 'João Pedro', 'Lauane Stefanny', 'Valter', font=10, text_color='white', title='Equipe' )
-    if window == janela3 and event == 'Documentação':
-        sg.popup('Documentação', 'Em breve!', font='18', title='Documentação')
-
+        sg.popup('Nossa equipe:','Adriano Rodrigues', 'Carlos Adriano', 'João Pedro', 'Lauane Stefanny', 'Valter Gomes', font=10, text_color='white', title='Equipe' )
 
     if window == janela3 and event == 'Continuar':
         janela3.hide()
@@ -76,45 +84,22 @@ while True:
         arquivo = open(arquivoCSV, "r", encoding='utf-8')
         df = pd.read_csv(arquivoCSV)
 
-
         #################DROPAR COLUNAS
-        df.drop('3. Informe os 7 últimos dígitos do seu RA: (109nnnxxxxxxx) ', inplace=True, axis=1)
-        df.drop('23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Em casa]', inplace=True, axis=1)
-        df.drop('23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [No trabalho]', inplace=True, axis=1)
-        df.drop('23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Na escola]', inplace=True, axis=1)
-        df.drop('23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Em outros lugares]', inplace=True,
-                axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para trabalhos profissionais:]',
-            inplace=True, axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para trabalhos escolares:]',
-            inplace=True,
-            axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para entretenimento (músicas, vídeos, redes sociais, etc):]',
-            inplace=True,
-            axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para comunicação por e-mail:]',
-            inplace=True,
-            axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para operações bancárias:]',
-            inplace=True,
-            axis=1)
-        df.drop(
-            '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para compras eletrônicas:]',
-            inplace=True,
-            axis=1)
-        df.drop(
-            '42. Escreva algumas linhas sobre sua história e seus sonhos de vida.',
-            inplace=True,
-            axis=1)
+        df.drop(['3. Informe os 7 últimos dígitos do seu RA: (109nnnxxxxxxx) ',
+                '23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Em casa]',
+                '23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [No trabalho]',
+                '23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Na escola]',
+                '23-1 .Onde você utiliza microcomputadores (notebooks e desktops)? [Em outros lugares]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para trabalhos profissionais:]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para trabalhos escolares:]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para entretenimento (músicas, vídeos, redes sociais, etc):]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para comunicação por e-mail:]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para operações bancárias:]',
+                '23-2.Com qual finalidade você utiliza microcomputadores (notebooks e desktops)? [Para compras eletrônicas:]',
+                '42. Escreva algumas linhas sobre sua história e seus sonhos de vida.'],
+                inplace=True, axis=1)
 
         df.rename(columns={df.columns[7]: '7. Em qual dia você nasceu?'}, inplace=True)
-
-
         ########TRATAR DADOS DA IDADE
         linhas = csv.reader(arquivo)
         listaDias = []
@@ -170,9 +155,7 @@ while True:
 
             data = dia, mes, ano
             listaDias.append(data)
-
             current_time = datetime.now()
-
         current_time = datetime.now()
         idade = []
 
@@ -180,8 +163,6 @@ while True:
             dataNew = (f'{tamanho[0]}/{tamanho[1]}/{tamanho[2]}')
             data = datetime.strptime(dataNew, '%d/%m/%Y')
             idade.append(calculateAge(data))
-
-
 
         ####CRIANDO A COLUNA IDADES
         df.insert(3, '3. Idades?', idade, allow_duplicates=True)
@@ -192,19 +173,16 @@ while True:
         janela2 = make_window()
 
     ###NAO USANDO POR ENQUANTO
-    elif window == janela1 and event == 'Continuar' and event == '-EXCEL-':
-        arquivoXML = values['-FILEBROWSE-']
-        df = pd.read_xml(arquivoXML)
-        listBox = df.columns[1:]
-        del listBox[0:5]
-        janela1.hide()
-        janela2 = make_window()
 
     # Retorna para interface anterior
     if window == janela2 and event == 'Voltar':
         janela2.close()
         janela1.un_hide()
 
+    if window == janela2 and event == '-FILTER-':
+        new_list = [i for i in listBox if values['-FILTER-'].lower() in i.lower()]
+        window['-LB-'].update(new_list)
+        window['-FILTER NUMBER-'].update(f'{len(new_list)} files')
 
     #GERANDO A LISTBOX(AS PERGUNTAS)
     if window == janela2 and event == '-LB-':
@@ -213,9 +191,10 @@ while True:
         contar = df[gerador]
 
         ##FORMATANDO O MATPLOIT(PARAMETROS)
+
         params = {
-            'legend.fontsize': 8,
-            'legend.loc': 'upper right',
+            'legend.fontsize': 10,
+            'legend.loc': 'upper left',
             'legend.framealpha': 0,
             'legend.handlelength': 1.0,
             'legend.handleheight': 0.7
@@ -231,13 +210,16 @@ while True:
         ##GERANDO O GRAFICO
         plt.pie(valoresGrafico, autopct="%1.2f%%")
         #MOSTRA OS TITULOS DOS GRAFICOS EM RELAÇÃO A LOCALIZAÇÃO
-        plt.title(titleGrafico)  ##Ok
+        plt.title(titleGrafico, size=10)  ##Ok
         ##CONTANDO AS LEGENDAS
         legenda = contar.value_counts()
         ##GERANDO AS LEGENDAS A PARTIR DO CONTADOR
-        plt.legend(valoresGrafico.index)
 
-    ###MOSTRAR O GRAFICO GERADO
-    plt.show()
-##
+        plt.legend(valoresGrafico.index, prop={'size': 7})
+
+        ##MOSTRANDO GRAFICO GERADO
+        plt.axis("equal")
+        plt.tight_layout()
+        plt.show()
+
 window.close()
